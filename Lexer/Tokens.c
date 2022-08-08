f504e6c2d40baa969fa0ccf2199d696e2d67fad7
poincianaLang <110737480+poicianaLang@users.noreply.github.com>
2022-08-07 20:27:21 -0500

    Update Tokens.c

--- Lexer/Tokens.c
+++ Lexer/Tokens.c
@@ -5,6 +5,6 @@ type Token = ""
   Token = {
   "if", "else", "elif", "catch", "end catch", "endif", "end", "include", "$print", "int", "class",
   "==", "!=", ">=", "=>, ";", ":", "{}", "[]", "()", "x++", "z++", "x--"', "z--", "+=", "newvar",
-    "func", ",", ".", "%", "\", "alpha", "beta", "gamma", "or", "and", "for"
+    "func", ",", ".", "%", "\", "alpha", "beta", "gamma", "or", "and", "for", "return", "void"
    }
 }
