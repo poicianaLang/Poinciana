98de5a51174654b215358b8195f90d2eda633f8d
poincianaLang <110737480+poicianaLang@users.noreply.github.com>
2022-08-10 12:35:18 -0500

    Delete info.bat

--- info.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Information For Poinciana
-echo Poinciana Is A Custom Programming Language
-echo It Is Currently In Development
-echo It Will Be Object Oriented And Will Have New Statements.
-pause
