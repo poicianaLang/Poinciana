d287544b072bf1b6f248e10e86c87a924d7bb85f
poincianaLang <110737480+poicianaLang@users.noreply.github.com>
2022-08-12 21:04:13 -0500

    Replaced Class With enum

--- Lexer/Tokens.cpp
+++ Lexer/Tokens.cpp
@@ -1,9 +1,9 @@
 int main()
 {
 
-enum {
+enum (
 "if", "else", "elif", "catch", "end catch", "endif", "end", "include", "$print", "int", "class",
   "==", "!=", ">=", "=<", ";", ":", "{}", "[]", "()", "x++", "z++", "+=", "newvar",
   "func", ",", ".", "%", "alpha", "beta", "gamma", "or", "and", "for", "return", "void"
-   }
+  )
 }
